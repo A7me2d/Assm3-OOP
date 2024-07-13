6825417ea3dbcc2714e4683de47ee4c8beb649c1
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-13 20:23:02 +0300

    OverLoading

--- Assm3 OOP/Program.cs
+++ Assm3 OOP/Program.cs
@@ -2,9 +2,74 @@
 {
     internal class Program
     {
+
+        #region OverLoading
+        public static int Sum(int x , int y)
+        {
+
+        return x + y; 
+        }
+    
+        public static int Sum(int x , int y , int z)
+        {
+            return x + y + z;
+        }
+
+        public static double Sum(int x, double y) { 
+        return x + y;
+        }
+        public static double Sum(double x, int y) { 
+        return x + y;
+        }
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+            #region Polymor phism (OverLoading)
+
+            //int Result = Sum(1, 2);
+            //Console.WriteLine(Result);
+
+            //Result = Sum(2, 3 , 4);
+            //Console.WriteLine(Result);
+
+            //double Result02 = Sum(3.2, 4);
+            //Console.WriteLine(Result);
+
+            #endregion
+
+
+            #region
+
+            #endregion 
+
+
+            #region
+
+            #endregion
+
+
+            #region
+
+            #endregion
+
+
+            #region
+
+            #endregion
+
+
+            #region
+
+            #endregion
+
+
         }
     }
 }
